cf5eae21bb8e0cf20ae0c071266eb47695062566
sakshi2894 <sakshib@qubole.com>
2017-12-12 21:38:44 +0530

    new:dev:#7:Add a unit test framework and use travis
    Fix #7

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+https://travis-ci.org/travis-ci/docs-travis-ci-com.svg?branch=master
+
 # caching-metastore-client
 A metastore client that caches objects
---
+++ src/test/java/com/qubole/utility/RedisCacheTest.java
@@ -0,0 +1,14 @@
+package com.qubole.utility;
+
+import org.junit.Test;
+
+public class RedisCacheTest {
+
+  @Test
+  public void dummyTest() {
+
+    assert (1==1);
+
+  }
+
+}
